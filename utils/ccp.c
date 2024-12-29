a4f5272c536c07136c4e6542e3f41013510f0782
SimonLiu <simonliu423@gmail.com>
2024-12-29 18:08:09 +0800

    Feat: add MotorRotateWithDelay

--- utils/ccp.c
+++ utils/ccp.c
@@ -2,6 +2,8 @@
 #include "timer.h"
 #include "config.h"
 
+double PWMDutyCycle = 0;
+
 void PWMInitialize(){
     TRISCbits.TRISC2 = 0;
     CCP1CONbits.CCP1M = 0b1100;
@@ -25,8 +27,29 @@ void PWMSetDutyCycle(double duty_cycle_us){
      * = (0x0b*4 + 0b01) * 8µs * 4
      * = 0.00144s ~= 1450µs
      */
+    PWMDutyCycle = duty_cycle_us;
     int prescaler = Timer2GetPrescaler();
-    long long val = (duty_cycle_us * _XTAL_FREQ / 1000000) / prescaler;
+    long long val = (PWMDutyCycle * _XTAL_FREQ / 1000000) / prescaler;
     CCPR1L = (val >> 2) & 0xFF;
     CCP1CONbits.DC1B = (val & 0x03);
+}
+
+double PWMGetDutyCycle(){
+    return PWMDutyCycle;
+}
+
+void MotorRotateWithDelay(double target_duty_cycle){
+    while(PWMDutyCycle != target_duty_cycle){
+        double next_duty_cycle;
+        if(PWMDutyCycle < target_duty_cycle){
+            next_duty_cycle = PWMDutyCycle + PWM_MOTOR_STRIDE;
+            if(next_duty_cycle > target_duty_cycle) next_duty_cycle = target_duty_cycle;
+            PWMSetDutyCycle(next_duty_cycle);
+        } else if(PWMDutyCycle > target_duty_cycle){
+            next_duty_cycle = PWMDutyCycle - PWM_MOTOR_STRIDE;
+            if(next_duty_cycle < target_duty_cycle) next_duty_cycle = target_duty_cycle;
+            PWMSetDutyCycle(next_duty_cycle);
+        }
+        __delay_ms(2);
+    }
 }
--- utils/ccp.h
+++ utils/ccp.h
@@ -3,9 +3,11 @@
 
 #include "settings.h"
 #define CCP_IF PIR1bits.CCP1IF
+#define PWM_MOTOR_STRIDE 15
 
 void PWMInitialize();
 void PWMSetPeriod(double period_ms);
 void PWMSetDutyCycle(double duty_cycle_us);
-
+double PWMGetDutyCycle();
+void MotorRotateWithDelay(double target_duty_cycle);
 #endif
